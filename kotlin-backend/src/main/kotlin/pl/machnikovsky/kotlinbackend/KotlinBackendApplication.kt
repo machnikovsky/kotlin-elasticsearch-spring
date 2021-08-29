073a4437f94db00a2b0720994299e6bc26df03b7
machnikovsky <fraczekjakub1999@gmail.com>
2021-08-29 16:32:57 +0200

    created dev and prod profiles

--- kotlin-backend/src/main/kotlin/pl/machnikovsky/kotlinbackend/KotlinBackendApplication.kt
+++ kotlin-backend/src/main/kotlin/pl/machnikovsky/kotlinbackend/KotlinBackendApplication.kt
@@ -1,9 +1,13 @@
 package pl.machnikovsky.kotlinbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import pl.machnikovsky.kotlinbackend.config.EsConfig
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class KotlinBackendApplication
 
 fun main(args: Array<String>) {
--- kotlin-backend/src/main/kotlin/pl/machnikovsky/kotlinbackend/config/EsConfig.kt
+++ kotlin-backend/src/main/kotlin/pl/machnikovsky/kotlinbackend/config/EsConfig.kt
@@ -1,25 +1,27 @@
-package pl.machnikovsky.kotlinbackend.config;
+package pl.machnikovsky.kotlinbackend.config
 
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.elasticsearch.client.RestHighLevelClient
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.client.ClientConfiguration
 import org.springframework.data.elasticsearch.client.RestClients
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = ["pl.machnikovsky.kotlinbackend.repository"])
 @ComponentScan(basePackages = ["pl.machnikovsky.kotlinbackend"])
-class EsConfig {
+class EsConfig(@Value("\${elastic.hostname}") val hostname: String,
+               @Value("\${elastic.port}") val  port: String) {
 
     @Bean
     fun client(): RestHighLevelClient {
         val clientConfiguration: ClientConfiguration = ClientConfiguration
             .builder()
-            .connectedTo("es01:9200")
+            .connectedTo("$hostname:$port")
             .build()
 
         return RestClients.create(clientConfiguration).rest()
